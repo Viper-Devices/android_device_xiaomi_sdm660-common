ad76f6c139a8ab96ba56e3a212529c7523dc0302
Venkat Yadlapati <venkat.yadlapati362@gmail.com>
2019-08-18 23:13:27 +0530

    sdm660-common: Remove Snap

--- platform.mk
+++ platform.mk
@@ -103,13 +103,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-	android.frameworks.displayservice@1.0_32 \
-	android.hardware.camera.provider@2.4-impl:32 \
-	android.hardware.camera.provider@2.4-service \
-	camera.device@3.2-impl \
-	libxml2 \
-	vendor.qti.hardware.camera.device@1.0 \
-	vendor.qti.hardware.camera.device@1.0_vendor
+    android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-service \
+    libxml2
 
 # CNE
 PRODUCT_PACKAGES += \
